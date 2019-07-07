89e0fd3d635051fce357229784641b558e602634
Basant Patidar <34193893+basantpatidar@users.noreply.github.com>
2019-07-06 23:46:22 -0400

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 // const KEY = "AIzaSyB_qnwH5HxTzpSQQpamcnpAdn6-dYvi-X4";
+//API is on the App.js page
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3"
